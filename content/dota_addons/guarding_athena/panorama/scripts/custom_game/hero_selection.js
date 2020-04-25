fcb922b7a913058e97d6023e43e5941a12426e0d
bigciba <584665414@qq.com>
2020-04-25 15:50:09 +0800

    选英雄js

--- content/dota_addons/guarding_athena/panorama/scripts/custom_game/hero_selection.js
+++ content/dota_addons/guarding_athena/panorama/scripts/custom_game/hero_selection.js
@@ -1,5 +1,17 @@
+let GameModeSelectionEndTime = -1;
 
-
+function Update() {
+	if (Game.GameStateIsAfter(DOTA_GameState.DOTA_GAMERULES_STATE_PRE_GAME)) {
+		$.GetContextPanel().DeleteAsync(-1);
+		return;
+	}
+	$.Schedule(0, Update);
+	
+	if (GameModeSelectionEndTime != -1) {
+		let time = GameModeSelectionEndTime - Game.GetGameTime();
+		$("#TimerContent").SetDialogVariableInt("time", Math.ceil(time));
+	}
+}
 (function () {
 	let HUD = $.GetContextPanel().GetParent().GetParent().GetParent();
 	let PreGame = HUD.FindChildTraverse("PreGame");
@@ -21,4 +33,29 @@
 	let Chat = PreGame.FindChildTraverse("Chat");
 	if (Chat)
 		Chat.style.opacity = "0";
+
+	GameModeSelectionEndTime = Game.GetGameTime() + Game.GetStateTransitionTime();
+	Update();
+	// 加载英雄卡片
+	let elements = $("#HeroListPanel").FindChildrenWithClassTraverse("HeroCard");
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].DeleteAsync(0);
+	}
+	for (const key in GameUI.HeroesKv) {
+		const HeroKV = GameUI.HeroesKv[key];
+		let HeroName = HeroKV.override_hero;
+		let Panel = $("#HeroListPanel").FindChildTraverse("HeroName");
+		if (Panel == undefined || Panel == null) {
+			let Panel = $.CreatePanelWithProperties("DOTAHeroMovie", $("#HeroListPanel"), HeroName, {heroname: HeroName});
+			Panel.BLoadLayoutSnippet("HeroCard");
+			if (HeroKV.UnitLabel == "lock" ) {
+				Panel.RemoveClass("Unlock");
+			}
+		}
+	}
+	// 加载玩家卡片
+	let elements = $("#HeroListPanel").FindChildrenWithClassTraverse("HeroCard");
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].DeleteAsync(0);
+	}
 })();
