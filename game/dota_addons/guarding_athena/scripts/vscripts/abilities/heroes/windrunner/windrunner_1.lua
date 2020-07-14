7086d34b24f5cd0b8f12930b12376691c745e937
bigciba <584665414@qq.com>
2020-07-15 01:53:40 +0800

    优化风行12技能

--- game/dota_addons/guarding_athena/scripts/vscripts/abilities/heroes/windrunner/windrunner_1.lua
+++ game/dota_addons/guarding_athena/scripts/vscripts/abilities/heroes/windrunner/windrunner_1.lua
@@ -3,68 +3,55 @@ LinkLuaModifier( "modifier_windrunner_1", "abilities/heroes/windrunner/windrunne
 if windrunner_1 == nil then
 	windrunner_1 = class({})
 end
-function windrunner_1:OnSpellStart()
+function windrunner_1:OnChannelFinish(bInterrupted)
+	local flChannelTime = GameRules:GetGameTime() - self:GetChannelStartTime()
 	local hCaster = self:GetCaster()
 	local vPosition = self:GetCursorPosition()
-	local vStart = hCaster:GetAbsOrigin()
-	local tPosition = {
-		vPosition,
-		RotatePosition(vStart, QAngle(0, 15, 0), vPosition),
-		RotatePosition(vStart, QAngle(0, 30, 0), vPosition),
-		RotatePosition(vStart, QAngle(0, -15, 0), vPosition),
-		RotatePosition(vStart, QAngle(0, -30, 0), vPosition)
-	}
-	self:MultipleShoot(tPosition, DAMAGE_TYPE_PHYSICAL)
-	if self:GetIntrinsicModifier():GetStackCount() > 0 then
-		hCaster:GameTimer(0.4, function ()
-			self:MultipleShoot(tPosition, DAMAGE_TYPE_MAGICAL)
-		end)
+	local flAngle = RemapVal(flChannelTime, 0, self:GetChannelTime(), 15, 5)
+	self:MultipleShoot(vPosition, flAngle, DAMAGE_TYPE_PHYSICAL)
+	-- 第二次魔法伤害多重射击
+	local delay = self:GetSpecialValueFor("delay")
+	hCaster:GameTimer(delay, function ()
+		self:MultipleShoot(hCaster:GetAbsOrigin() + hCaster:GetForwardVector(), flAngle, DAMAGE_TYPE_MAGICAL)
+	end)
+	-- 冷却
+	local flCooldown = self:GetCooldownTimeRemaining()
+	local flCooldownReduction = RemapVal(flChannelTime, 0, self:GetChannelTime(), self:GetSpecialValueFor("cooldown_reduction") * 0.01, 0)
+	if flCooldownReduction > 0 then
+		self:EndCooldown()
+		self:StartCooldown(flCooldown * (1 - flCooldownReduction))
 	end
 end
-function windrunner_1:MultipleShoot(tPosition, iDamageType)
+function windrunner_1:MultipleShoot(vPosition, flAngle, iDamageType)
 	local hCaster = self:GetCaster()
 	local vStart = hCaster:GetAbsOrigin()
 	local flRadius = self:GetSpecialValueFor("radius")
 	local flDistance = self:GetSpecialValueFor("range")
 	local iSpeed = self:GetSpecialValueFor("speed")
 	local sParticleName = iDamageType == DAMAGE_TYPE_PHYSICAL and "particles/units/heroes/hero_windrunner/windrunner_spell_powershot.vpcf" or "particles/skills/multiple_shoot_magic.vpcf"
+	local tPosition = {
+		vPosition,
+		RotatePosition(vStart, QAngle(0, flAngle, 0), vPosition),
+		RotatePosition(vStart, QAngle(0, flAngle * 2, 0), vPosition),
+		RotatePosition(vStart, QAngle(0, -flAngle, 0), vPosition),
+		RotatePosition(vStart, QAngle(0, -flAngle * 2, 0), vPosition)
+	}
 	for i, vTargetLoc in ipairs(tPosition) do
 		local vDirection = (vTargetLoc - vStart):Normalized()
 		CreateLinearProjectile(hCaster, self, sParticleName, vStart, flRadius, flDistance, vDirection, iSpeed, false, {iDamageType = iDamageType})
 	end
 	-- 放箭动作
-	hCaster:ForcePlayActivityOnce(ACT_DOTA_OVERRIDE_ABILITY_2)
+	hCaster:StartGesture(ACT_DOTA_OVERRIDE_ABILITY_2)
 end
 function windrunner_1:OnProjectileHit_ExtraData(hTarget, vLocation, ExtraData)
 	if hTarget then
 		local hCaster = self:GetCaster()
-		local flDamage = self:GetSpecialValueFor("base_damage") + hCaster:GetAgility() * self:GetSpecialValueFor("damage")
+		local flRange = self:GetSpecialValueFor("range")
+		local flDistance = RemapVal(CalculateDistance(hTarget, hCaster), 0, flRange, flRange * 0.5, 0)
+		local flDamage = self:GetSpecialValueFor("base_damage") + hCaster:GetAverageTrueAttackDamage(hTarget) * self:GetSpecialValueFor("damage")
 		local iDamageType = ExtraData.iDamageType
 		hCaster:DealDamage(hTarget, self, flDamage, iDamageType)
+		-- 击退
+		hCaster:KnockBack(hCaster:GetAbsOrigin() + hCaster:GetForwardVector() * -hCaster:GetHullRadius(), hTarget, flDistance, 0, 0.3, false)
 	end
-end
-function windrunner_1:GetIntrinsicModifierName()
-	return "modifier_windrunner_1"
-end
----------------------------------------------------------------------
---Modifiers
-if modifier_windrunner_1 == nil then
-	modifier_windrunner_1 = class({}, nil, ModifierBasic)
-end
-function modifier_windrunner_1:OnCreated(params)
-	if IsServer() then
-		self:SetStackCount(3)
-	end
-end
-function modifier_windrunner_1:OnRefresh(params)
-	if IsServer() then
-	end
-end
-function modifier_windrunner_1:OnDestroy()
-	if IsServer() then
-	end
-end
-function modifier_windrunner_1:DeclareFunctions()
-	return {
-	}
 end
